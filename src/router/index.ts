fc728f5fb7de32d9a3e72b45b453bfc6838e3369
João Vitor <jvitordiass@outlook.com>
2024-12-31 17:37:55 -0300

    chore: remove `vite-plugin-vue-layouts`

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes)
+  routes: routes
 })
 
 router.onError((err, to) => {
